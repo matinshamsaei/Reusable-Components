c1d11fd454a7e41be8aa5d88cbfe1bd27fb25b39
ardalan shahandeh <ardalan.shahande@gmail.com>
2023-02-20 14:34:38 +0330

    feat: Add CNumericDisplay Component To Share Component Folder

---
+++ src/components/NumericDisplay/index.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+type Props = {
+    value?: number
+    direction?:string
+    showAppend?:boolean
+    showSeperated?: boolean
+    append?: string
+    label?:string
+    labelClass?: object | string
+    amountClass?: object | string
+    appendClass?:object | string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    direction: 'ltr',
+    showAppend: true,
+    showSeperated: true
+})
+
+const amount = computed(()=> {
+    if (!props.showSeperated) return props.value
+    else if (props.value !== null) return props.value?.toLocaleString()
+    else return null
+})
+
+const isInfinity = computed(()=>{
+    return props.value === 2147483647
+})
+
+</script>
+
+<template>
+  <span>
+    <span v-if="props.label" class="d-inline-block" :class="labelClass">
+      {{ props.label }}
+    </span>
+
+    <font-awesome-icon id="infinity-icon" v-if="isInfinity" icon="infinity" class="text-muted align-middle" />
+
+    <span v-else>
+      <span :dir="props.direction" :class="amountClass">{{ amount }}</span>
+
+      <span v-if="props.showAppend && props.append" :class="appendClass"> {{ props.append }}</span>
+    </span>
+  </span>
+</template>
