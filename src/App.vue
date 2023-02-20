41bf35ce9bdb373ac13aadde527f18951089f491
alireza yegane <alirezaporyegane@gmail.com>
2023-02-20 12:33:48 +0330

    fix: Clean App Components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
-<script setup lang="ts">
-import useGlobalProps from './composable/useGlobalProps'
-import { RLink, RImg } from '@routaa/ui-kit'
-import Thumbnail from './components/Thumbnail/index.vue'
+<script setup lang="ts"></script>
 
-const globalProps = useGlobalProps()
-
-console.log(globalProps.$config)
-</script>
-
-<template>
-  welcome to shared components
-  <RImg src="http://192.168.7.16:800/unsafe/fit-in/business/files/site-widget/header/47013ec539c143afab2b8445a306fb3b.png"/>
-  <Thumbnail src="/site-widget/header/47013ec539c143afab2b8445a306fb3b.png" />
-</template>
+<template>welcome to shared components</template>
