061addccfaaacf5cb8a8675c764de4acaf8003ae
Amir Nasiri <amirnasiri9473@gmail.com>
2023-02-22 12:17:41 +0330

    feat: Add Page500 Component

--- src/translations/fa/shared.ts
+++ src/translations/fa/shared.ts
@@ -1,10 +1,13 @@
 export default {
   add: 'افزودن',
-  cancel: 'انصراف'
+  cancel: 'انصراف',
   email: 'ایمیل',
   excelExport: 'خروجی اکسل',
+  loadAgain: 'بارگیری مجدد',
   mobile: 'موبایل',
   noRecords: 'رکوردی برای نمایش وجود ندارد',
+  oopsWeHaveAProblem: 'مشکلی پیش آمده!',
   reportDate: 'تاریخ گزارش',
   save: 'ذخیره',
+  tryAgain: 'دوباره امتحان کنید'
 }
