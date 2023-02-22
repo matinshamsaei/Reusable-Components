3988003dad4c405451b0628fcaa060da233a22b1
matin <matin3061379@gmail.com>
2023-02-22 12:33:58 +0330

    feat: Implement Base Of ErrorsDisplay

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
-<script setup lang="ts"></script>
+<template>
+  <div class="container my-5">
+    <RFormInput v-model="email" name="email" placeholder="email" class="mb-3" />
+    <RFormInput v-model="password" type="password" name="password" placeholder="password" />
+  </div>
+</template>
 
-<template>welcome to shared components</template>
+<script setup>
+import {ref} from 'vue'
+import { useForm, useField } from 'vee-validate'
+import * as yup from 'yup'
+import { RFormInput } from '@routaa/ui-kit'
+
+const email = ref('')
+const password = ref('')
+
+const schema = yup.object({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(8)
+})
+
+const { handleSubmit } = useForm({
+  validationSchema: schema
+})
+</script>
---
+++ src/components/ErrorsDisplay/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RAlert } from '@routaa/ui-kit'
+import { computed } from 'vue'
+
+type Props = {
+  value?: object[] | object
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <pre>
+        {{ props.value }}
+    </pre
+  >
+
+  <!-- <RAlert :show="show" id="errors" variant="danger" class="px-4">
+    <ul class="m-0 px-1">
+      <li v-if="$slots.first"><slot name="first"></slot></li>
+
+      <li v-for="err in errorItems.all(form)" :key="err" v-html="err" />
+
+      <template v-if="extraErrors && extraErrors.length">
+        <li v-for="err in extraErrors" :key="err" v-html="err" />
+      </template>
+
+      <li v-if="$slots.last"><slot name="last"></slot></li>
+    </ul>
+  </RAlert> -->
+</template>
