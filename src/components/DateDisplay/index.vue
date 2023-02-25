f4295c8e8edf8af7d7ab3dfb56f6f3dd1ee2042f
ardalan shahandeh <ardalan.shahande@gmail.com>
2023-02-25 12:32:50 +0330

    style: Clean Code

--- src/components/DateDisplay/index.vue
+++ src/components/DateDisplay/index.vue
@@ -1,91 +1,3 @@
-<template>
-  <span>
-    <span v-if="props.label" class="d-inline-block" :class="props.labelClass">
-      {{ props.label }}
-    </span>
-
-    <span v-if="props.modelValue && props.relative" id="relative-time">
-      <font-awesome-icon
-        v-if="props.icon"
-        icon="calendar-days"
-        :size="props.iconSize"
-        class="me-1"
-        :class="props.iconClass"
-      />
-      {{ relativeTime }}
-    </span>
-
-    <span v-else-if="props.modelValue && props.humanize" id="humanized-time">
-      <font-awesome-icon
-        v-if="props.icon"
-        icon="calendar-days"
-        :size="props.iconSize"
-        class="me-1"
-        :class="props.iconClass"
-      />
-      {{ humanizedTime }}
-    </span>
-
-    <span v-else-if="props.modelValue" id="date">
-      <template v-if="isFinity">
-        <span v-if="props.time && props.firstIsDate" class="me-2">
-          <font-awesome-icon
-            v-if="props.icon"
-            icon="clock"
-            :size="props.iconSize"
-            class="me-1"
-            :class="props.iconClass"
-          />
-
-          <span v-if="props.seconds" id="time-vs">
-            {{ timeVS }}
-          </span>
-
-          <span v-else id="time-v">
-            {{ timeV }}
-          </span>
-        </span>
-
-        <template v-if="props.date">
-          <font-awesome-icon
-            v-if="props.icon"
-            icon="calendar-days"
-            :size="props.iconSize"
-            class="me-1"
-            :class="props.iconClass"
-          />
-
-          <span id="get-day" v-if="props.dayName" class="me-2"> {{ getDayName }} </span>
-
-          {{ dateV }}
-        </template>
-
-        <span v-if="props.time && !props.firstIsDate" class="ms-2">
-          <font-awesome-icon
-            v-if="props.icon"
-            icon="clock"
-            :size="props.iconSize"
-            class="me-1"
-            :class="props.iconClass"
-          />
-
-          <span v-else> {{ t('shared.clock') }} </span>
-
-          <span v-if="props.seconds" id="time-vs">
-            {{ timeVS }}
-          </span>
-
-          <span v-else id="time-v">
-            {{ timeV }}
-          </span>
-        </span>
-      </template>
-
-      <font-awesome-icon v-else icon="infinity" id="icon-infinity" class="align-middle text-muted" />
-    </span>
-  </span>
-</template>
-
 <script setup lang="ts">
 import momentTimezone from 'moment-timezone'
 import useGlobalProps from '../../composable/useGlobalProps'
@@ -196,3 +108,91 @@ const humanizedTime = computed(() => {
   }
 })
 </script>
+
+<template>
+  <span>
+    <span v-if="props.label" class="d-inline-block" :class="props.labelClass">
+      {{ props.label }}
+    </span>
+
+    <span v-if="props.modelValue && props.relative" id="relative-time">
+      <font-awesome-icon
+        v-if="props.icon"
+        icon="calendar-days"
+        :size="props.iconSize"
+        class="me-1"
+        :class="props.iconClass"
+      />
+      {{ relativeTime }}
+    </span>
+
+    <span v-else-if="props.modelValue && props.humanize" id="humanized-time">
+      <font-awesome-icon
+        v-if="props.icon"
+        icon="calendar-days"
+        :size="props.iconSize"
+        class="me-1"
+        :class="props.iconClass"
+      />
+      {{ humanizedTime }}
+    </span>
+
+    <span v-else-if="props.modelValue" id="date">
+      <template v-if="isFinity">
+        <span v-if="props.time && props.firstIsDate" class="me-2">
+          <font-awesome-icon
+            v-if="props.icon"
+            icon="clock"
+            :size="props.iconSize"
+            class="me-1"
+            :class="props.iconClass"
+          />
+
+          <span v-if="props.seconds" id="time-vs">
+            {{ timeVS }}
+          </span>
+
+          <span v-else id="time-v">
+            {{ timeV }}
+          </span>
+        </span>
+
+        <template v-if="props.date">
+          <font-awesome-icon
+            v-if="props.icon"
+            icon="calendar-days"
+            :size="props.iconSize"
+            class="me-1"
+            :class="props.iconClass"
+          />
+
+          <span id="get-day" v-if="props.dayName" class="me-2"> {{ getDayName }} </span>
+
+          {{ dateV }}
+        </template>
+
+        <span v-if="props.time && !props.firstIsDate" class="ms-2">
+          <font-awesome-icon
+            v-if="props.icon"
+            icon="clock"
+            :size="props.iconSize"
+            class="me-1"
+            :class="props.iconClass"
+          />
+
+          <span v-else> {{ t('shared.clock') }} </span>
+
+          <span v-if="props.seconds" id="time-vs">
+            {{ timeVS }}
+          </span>
+
+          <span v-else id="time-v">
+            {{ timeV }}
+          </span>
+        </span>
+      </template>
+
+      <font-awesome-icon v-else icon="infinity" id="icon-infinity" class="align-middle text-muted" />
+    </span>
+  </span>
+</template>
