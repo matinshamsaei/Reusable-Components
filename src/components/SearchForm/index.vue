e82c9c6e33b50667dc01df4b02c94d4b5ba5c38b
alireza yegane <alirezaporyegane@gmail.com>
2023-02-26 09:18:14 +0330

    refactor: Delete Extra Props

--- src/components/SearchForm/index.vue
+++ src/components/SearchForm/index.vue
@@ -6,10 +6,8 @@ import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 
-
 type Props = {
   search?: boolean
-  img?: string
   icon?: string
   title?: string
 }
@@ -27,7 +25,7 @@ const searchTitle = computed(() => {
 <template>
   <div class="row d-flex justify-content-between align-items-center mb-4 pt-1">
     <div class="col-12 col-sm-6">
-      <PageTitle :title="searchTitle" :icon="props.icon" />
+      <PageTitle :title="searchTitle" :icon="props.icon" :simple="false" :noTopSpace="false" />
     </div>
 
     <div class="d-flex justify-content-end col-12 col-sm-6">
@@ -41,6 +39,7 @@ const searchTitle = computed(() => {
         class="text-nowrap px-4 ms-3"
       >
         <font-awesome-icon icon="filter" size="sm" class="align-middle me-1" />
+
         {{ t('shared.filter') }}
       </RButton>
     </div>
