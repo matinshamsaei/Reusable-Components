c83c40924e5e1026d9fbe8492ad99d6f485e8a8e
alireza yegane <alirezaporyegane@gmail.com>
2023-04-09 14:35:37 +0430

    refactor: Logic

--- src/components/TableFooter/index.vue
+++ src/components/TableFooter/index.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
 import { RFormSelect, RPagination } from '@routaa/ui-kit'
 import { computed, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-
-const { t } = useI18n()
-
-const sizeOptions = ref([10, 20, 50, 100])
-
-const emit = defineEmits<Emits>()
-
-interface Emits {
-  (e: 'update:currentPage', value: any): void
-  (e: 'update:perPage', value: any): void
-}
 
 type Props = {
   total: number | string
@@ -28,6 +16,15 @@ const props = withDefaults(defineProps<Props>(), {
   showPerPage: true,
   card: true
 })
+
+interface Emits {
+  (e: 'update:currentPage', value: any): void
+  (e: 'update:perPage', value: any): void
+}
+
+const emit = defineEmits<Emits>()
+
+const sizeOptions = ref([10, 20, 50, 100])
 
 const perPage = +props.perPage
 const currentPage = +props.currentPage
@@ -61,20 +58,17 @@ const from = computed(() => {
 })
 
 const to = computed(() => {
-  if (isLastPage) {
-    return props.total
-  }
+  if (isLastPage) return props.total
+
   return perPage * currentPage
 })
 
 function checkPagination(perPage: number) {
   let to
   let from
-  if (typeof props.total === 'number' && perPage * currentPage >= props.total) {
-    to = props.total
-  } else {
-    to = perPage * currentPage
-  }
+  if (typeof props.total === 'number' && perPage * currentPage >= props.total) to = props.total
+  else to = perPage * currentPage
+
   from = (currentPage - 1) * perPage + 1
   return from > to
 }
@@ -90,10 +84,10 @@ function checkPagination(perPage: number) {
     <div class="row d-flex justify-content-between">
       <div v-if="props.showTotal" class="col-sm col-12 d-flex align-items-center text-dark mb-3 ms-2 my-sm-1">
         <div class="mx-auto mx-sm-0">
-          <h6 class="d-inline-block font-md mb-0" id="from-to">{{ from }} {{ t('shared.until') }} {{ to }}</h6>
+          <h6 id="from-to" class="d-inline-block font-md mb-0">{{ from }} {{ $t('shared.until') }} {{ to }}</h6>
 
           <span>
-            {{ t('shared.from') }}
+            {{ $t('shared.from') }}
           </span>
 
           <h6 class="d-inline-block font-md mb-0">
@@ -101,7 +95,7 @@ function checkPagination(perPage: number) {
           </h6>
 
           <span>
-            {{ t('shared.item') }}
+            {{ $t('shared.item') }}
           </span>
         </div>
       </div>
@@ -109,19 +103,19 @@ function checkPagination(perPage: number) {
       <div v-if="props.total > props.perPage" class="d-flex my-auto col-sm col-12">
         <RPagination
           v-model="page"
+          class="font-md mb-0 mx-auto px-0"
           :totalRows="props.total"
           :perPage="props.perPage"
-          class="font-md mb-0 mx-auto px-0"
         />
       </div>
 
       <div v-if="props.showPerPage" class="d-flex me-1 my-2 col-sm col-12">
         <div class="d-flex align-items-center font-md mx-auto me-md-0 ms-md-auto">
           <div class="text-nowrap text-dark px-2">
-            {{ t('shared.count') }}
+            {{ $t('shared.count') }}
           </div>
 
-          <RFormSelect v-model="size" :options="sizeOptions" size="sm" class="text-dark transparent-input" />
+          <RFormSelect v-model="size" size="sm" class="text-dark transparent-input" :options="sizeOptions" />
         </div>
       </div>
     </div>
